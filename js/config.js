bf4fa164b864a44e1f0bc838d760e9c365a4fd26
excelglowish2026 <excelglowish2026@gmail.com>
2026-08-31 10:24:50 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Paste the URL you get from Apps Script > Deploy > Web app here.
 // It looks like: https://script.google.com/macros/s/AKfycb.../exec
 const APP_CONFIG = {
-  API_URL: 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE'
+  API_URL: 'https://script.google.com/macros/s/AKfycbx7-yuKsFoMVK0QhE6z1jU_w5q6kXdRXmlNFLpFGCNBj4vgKPrOCeCt5Eusg6szAKSSkg/exec'
 };
